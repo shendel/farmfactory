e8abda05fdc9f035b1fe814a345dc4d6de9b423d
Pavel Ivanov <grammka@gmail.com>
2021-01-04 20:25:53 +0300

    update farmfactory.php

--- farmfactory.php
+++ farmfactory.php
@@ -24,11 +24,11 @@ require __DIR__ . '/App/autoload.php';
  * Never worry about cache again!
  */
 function farm_load_scripts($hook) {
- 
+
     // create my own version codes
     $my_js_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'src/build/app.js' ));
     wp_enqueue_script( 'custom_js', plugins_url( 'lib/index.js', __FILE__ ), array(), $my_js_ver );
- 
+
 }
 add_action('wp_enqueue_scripts', 'farm_load_scripts');
 
@@ -40,15 +40,14 @@ function farmfactory_main_shortcode( $atts ){
 	 return "<div id=\"farmfactory-main-root\"></div><div id=\"farmfactory-farming-root\"></div>
 
   <script>
-    window.farmAddress = '".get_option("farmfactory_farmAddress","0x38054641b795fb9604961b4c18b871f42bf8afb0")."';
-    window.rewardsAddress = '".get_option("farmfactory_rewardsAddress","0x93d83a81905a1baf4615bcb51db3f2f2bbf6ab9e")."';
-    window.stakingAddress = '".get_option("farmfactory_stakingAddress","0xc3eC8ED5Ce2a19CA40210002116712645dBEceC4")."';
-	window.networkId = ".get_option("farmfactory_networkId","42").";
-  </script>
-  <script>
-    farmFactory.init()
+    farmFactory.init({
+      networkId: ".get_option("farmfactory_networkId","42").",
+      farmAddress: '".get_option("farmfactory_farmAddress","0x38054641b795fb9604961b4c18b871f42bf8afb0")."',
+      rewardsAddress: '".get_option("farmfactory_rewardsAddress","0x93d83a81905a1baf4615bcb51db3f2f2bbf6ab9e")."',
+      stakingAddress: '".get_option("farmfactory_stakingAddress","0xc3eC8ED5Ce2a19CA40210002116712645dBEceC4")."'
+    });
   </script>";
 }
 
-// результат: 
+// результат:
 // шоткод [footag foo="bar"] в тексте будет заменен на "foo = bar"
