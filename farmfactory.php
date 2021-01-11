d9856b7c172509f8d889245a2d105244d388d2ef
Noxon <2914674+noxonsu@users.noreply.github.com>
2021-01-11 20:05:10 +0300

    Update farmfactory.php

--- farmfactory.php
+++ farmfactory.php
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
 define( 'FARMFACTORY_TEMPLATE_DIR', __DIR__ . '/templates' );
 define( 'FARMFACTORY_BASE_DIR', __DIR__ );
 define( 'FARMFACTORY_BASE_FILE', __FILE__ );
-define( 'FARMFACTORY_VER ', "1.0.132");
+define( 'FARMFACTORY_VER', "1.0.133");
 /**
  * Plugin Init
  */
