76a0954bb146b2bfc2b338be92328b7afd9ea647
Noxon <2914674+noxonsu@users.noreply.github.com>
2021-01-12 11:49:13 +0300

    Update HomePageController.php

--- App/Controllers/HomePageController.php
+++ App/Controllers/HomePageController.php
@@ -17,12 +17,6 @@ class HomePageController extends Controller {
 	}
 
 	public function template($template) {
-		/*
-        if ( is_front_page() || is_home() ) {
-
-            return FARMFACTORY_TEMPLATE_DIR  .'/home.php';
-        }
-		*/
 
         return $template;
 	}
