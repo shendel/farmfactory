9a0a05d8f4727ce73d88a38e49df868eab032d7c
Pavel Ivanov <grammka@gmail.com>
2021-04-10 13:44:24 +0300

    update deployer

--- src/deployer/index.ts
+++ src/deployer/index.ts
@@ -1,14 +1,14 @@
 import json from '../../contracts/Farm.json'
 import formatAmount from './formatAmount'
+import { injectModalsRoot } from './modals'
+import infoModal from './infoModal'
+import connectModal from './connectModal'
+import { accountUnlockedStorageKey } from './constants'
+import { getState, setState } from './state'
+import setupWeb3 from './setupWeb3'
 
 
 const rewardsAbi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"string"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"value","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"from","type":"address"}],"name":"getAvailableBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"tokensMinted","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"value","type":"uint256"}],"name":"burnFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"addMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"renounceMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isMinter","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"freezeFor","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"freezeOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_unfreezeTimestamp","type":"uint256"},{"name":"_subsequentUnlock","type":"bool"}],"name":"mintWithFreeze","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"maxSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterRemoved","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]
-
-const networks = {
-  mainnet: 'https://mainnet.infura.io/v3/5ffc47f65c4042ce847ef66a3fa70d4c',
-  ropsten: 'https://ropsten.infura.io/v3/5ffc47f65c4042ce847ef66a3fa70d4c',
-  kovan: 'https://kovan.infura.io/v3/5ffc47f65c4042ce847ef66a3fa70d4c',
-}
 
 const loadScript = (src) => new Promise((resolve, reject) => {
   const script = document.createElement('script')
@@ -18,43 +18,6 @@ const loadScript = (src) => new Promise((resolve, reject) => {
   script.src = src
 
   document.head.appendChild(script)
-})
-
-
-type Opts = {
-  onStartLoading: () => void
-  onFinishLoading: () => void
-  onError: (error: Error) => void
-}
-
-type State = {
-  opts: Opts
-  web3: any
-}
-
-const state: State = {
-  opts: null,
-  web3: null,
-}
-
-const setupWeb3 = () => new Promise((resolve, reject) => {
-  const activeNetworkName = ({
-    1: 'mainnet',
-    3: 'ropsten',
-    42: 'kovan',
-  })[window.ethereum && window.ethereum.networkVersion]
-
-  const network = networks[activeNetworkName]
-
-  const web3 = new window.Web3(window.ethereum || window.Web3.givenProvider || new window.Web3.providers.HttpProvider(network))
-
-  if (web3) {
-    state.web3 = web3
-    resolve()
-  }
-  else {
-    reject()
-  }
 })
 
 type Params = {
@@ -69,12 +32,13 @@ type Params = {
 const deploy = async (params: Params) => {
   const { abi, bytecode } = json
   const { rewardsAddress, stakingAddress, duration, decimal } = params
+  const { web3 } = getState()
 
   let contract
   let accounts
 
   try {
-    contract = new state.web3.eth.Contract(abi)
+    contract = new web3.eth.Contract(abi)
     accounts = await window.ethereum.request({ method: 'eth_accounts' })
   }
   catch (err) {
@@ -92,7 +56,7 @@ const deploy = async (params: Params) => {
     .send({
       from: accounts[0],
       gas: 3000000,
-    }, function(error, transactionHash) {})
+    }, (error, transactionHash) => {})
     .on('error', function(error) {
       if (typeof params.onError === 'function') {
         params.onError(error)
@@ -107,10 +71,11 @@ const deploy = async (params: Params) => {
 
 const startFarming = async ({ farmAddress, rewardsAddress, amount, onSuccess, onError }) => {
   const { abi } = json
+  const { web3 } = getState()
 
   try {
-    const farmContract = new state.web3.eth.Contract(abi, farmAddress)
-    const rewardsContract = new state.web3.eth.Contract(rewardsAbi, rewardsAddress)
+    const farmContract = new web3.eth.Contract(abi, farmAddress)
+    const rewardsContract = new web3.eth.Contract(rewardsAbi, rewardsAddress)
 
     const accounts = await window.ethereum.request({ method: 'eth_accounts' })
     const rewardsDecimals = await rewardsContract.methods.decimals().call()
@@ -135,16 +100,78 @@ const stopFarming = () => {
 }
 
 const handleError = (err) => {
-  if (typeof state.opts.onError === 'function') {
-    state.opts.onError(err)
+  const { opts } = getState()
+
+  if (typeof opts.onError === 'function') {
+    opts.onError(err)
   }
 }
 
-const init = async (opts: Opts) => {
-  state.opts = opts
+const initMetamask = async () => {
+  const isAccountUnlocked = localStorage.getItem(accountUnlockedStorageKey) === 'true'
 
-  if (typeof state.opts.onStartLoading === 'function') {
-    state.opts.onStartLoading()
+  if (isAccountUnlocked) {
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' })
+
+      if (!accounts[0]) {
+        localStorage.removeItem(accountUnlockedStorageKey)
+        connectModal.open()
+      }
+      else {
+        setState({ account: accounts[0] })
+
+        await setupWeb3()
+
+        const { opts } = getState()
+
+        if (typeof opts.onFinishLoading === 'function') {
+          opts.onFinishLoading()
+        }
+      }
+    }
+    catch (err) {
+      console.error(err)
+      localStorage.removeItem(accountUnlockedStorageKey)
+      connectModal.open()
+    }
+  }
+  else {
+    connectModal.open()
+  }
+}
+
+const connectMetamask = async () => {
+  if (!window.ethereum) {
+    infoModal.open({
+      message: `
+        <div class="install-metamask">
+          <img src="https://swaponline.github.io/images/metamask_45038d.svg" /><br />
+          Please install MetaMask
+        </div>
+      `,
+    })
+
+    return Promise.reject()
+  }
+
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      if (window.ethereum.networkVersion) {
+        clearInterval(interval)
+        initMetamask()
+        window.ethereum.on('networkChanged', initMetamask)
+        resolve()
+      }
+    }, 500)
+  })
+}
+
+const init = async (opts) => {
+  setState({ opts })
+
+  if (typeof opts.onStartLoading === 'function') {
+    opts.onStartLoading()
   }
 
   try {
@@ -152,11 +179,10 @@ const init = async (opts: Opts) => {
       loadScript('https://cdnjs.cloudflare.com/ajax/libs/web3/1.3.1/web3.min.js'),
       loadScript('https://cdnjs.cloudflare.com/ajax/libs/bignumber.js/8.0.2/bignumber.min.js'),
     ])
-    await setupWeb3()
 
-    if (typeof state.opts.onFinishLoading === 'function') {
-      state.opts.onFinishLoading()
-    }
+    injectModalsRoot()
+
+    await connectMetamask()
   }
   catch (err) {
     handleError(err)
