feb7128b59ab0b987239d57e4a71a015e5c214d3
BootWP <burdianov@gmail.com>
2021-04-13 21:08:33 +0300

    Update version 2.0.9

--- farmfactory.php
+++ farmfactory.php
@@ -6,7 +6,7 @@ Author: Denis Ivanov
 Requires PHP: 7.1
 Text Domain: farm
 Domain Path: /lang
-Version: 2.0.8
+Version: 2.0.9
  */
 /* Define Plugin Constants */
 defined( 'ABSPATH' ) || exit;
@@ -15,7 +15,7 @@ define( 'FARMFACTORY_BASE_DIR', __DIR__ );
 define( 'FARMFACTORY_BASE_FILE', __FILE__ );
 define( 'FARMFACTORY_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FARMFACTORY_URL', plugin_dir_url( __FILE__ ) );
-define( 'FARMFACTORY_VER', '2.0.8');
+define( 'FARMFACTORY_VER', '2.0.9');
 /**
  * Plugin Init
  */
